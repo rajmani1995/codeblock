6809818fb3c585bb2d41bce548a4fce6de589d2e
Rajmani Arya <rajmani1995@gmail.com>
2017-02-02 14:21:56 +0530

    Changed Templates and Settings

--- project/settings.py
+++ project/settings.py
@@ -108,5 +108,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ('./static',)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
